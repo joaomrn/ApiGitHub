be8623a39b7d56cf86cea8c17cf793b48446d564
João Vitor <joaovitormrn@gmail.com>
2020-08-16 12:47:54 -0300

    Correções no código.

--- ApiGitHub/Controllers/GitHubController.cs
+++ ApiGitHub/Controllers/GitHubController.cs
@@ -14,21 +14,19 @@ namespace ApiGitHub.Controllers
     [ApiController]
     public class GitHubController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get()
-        
-        
+        [HttpGet("{conta}")]
+        public IActionResult Get(string conta)
         {
+            if (string.IsNullOrEmpty(conta)) return BadRequest();
+
             var client = new RestClient("https://api.github.com/users/");
-            var request = new RestRequest("joaomrn/repos");
-            //request.AddParameter("valor", valor);
-            //request.RequestFormat = DataFormat.Json;
+            var request = new RestRequest(@$"{conta}/repos");
             var response = client.Get(request);
             var content = response.Content;
             var retorno = JsonConvert.DeserializeObject<IEnumerable<MyArray>>(content);
 
             var listaRepositorios = retorno.
-                OrderBy(x => x.Created_at)//Ordena pela lista data mais antiga
+                OrderBy(x => x.Created_at)//Ordena pela data mais antiga da lista
                 .ToList()//Converte em um List
                 .GetRange(0, 5);//Retorna os 5 primeiros elementos
 
