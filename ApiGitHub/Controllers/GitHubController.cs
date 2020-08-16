e803ce3c484546b04b56e76618458b434562a967
João Vitor <joaovitormrn@gmail.com>
2020-08-16 11:25:26 -0300

    Desenvolvido logica para obter os 5 repositorios mais antigos.

--- ApiGitHub/Controllers/GitHubController.cs
+++ ApiGitHub/Controllers/GitHubController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiGitHub.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace ApiGitHub.Controllers
 {
@@ -11,5 +14,24 @@ namespace ApiGitHub.Controllers
     [ApiController]
     public class GitHubController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get()
+        
+        {
+            var client = new RestClient("https://api.github.com/users/");
+            var request = new RestRequest("joaomrn/repos");
+            //request.AddParameter("valor", valor);
+            //request.RequestFormat = DataFormat.Json;
+            var response = client.Get(request);
+            var content = response.Content;
+            var retorno = JsonConvert.DeserializeObject<IEnumerable<MyArray>>(content);
+
+            var listaRepositorios = retorno.
+                OrderBy(x => x.created_at)//Ordena pela lista data mais antiga
+                .ToList()//Converte em um List
+                .GetRange(0, 5);//Retorna os 5 primeiros elementos
+
+            return new ObjectResult(listaRepositorios);
+        }
     }
 }
---
+++ ApiGitHub/Model/MyArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGitHub.Model
+{
+    public class MyArray
+    {
+        public int id { get; set; }
+        public string node_id { get; set; }
+        public string name { get; set; }
+        public string full_name { get; set; }
+        public GitHub GitHub { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        public DateTime pushed_at { get; set; }
+        public string git_url { get; set; }
+        public string ssh_url { get; set; }
+        public string clone_url { get; set; }
+        public string svn_url { get; set; }
+        public string homepage { get; set; }
+        public int size { get; set; }
+        public int stargazers_count { get; set; }
+        public int watchers_count { get; set; }
+        public string language { get; set; }
+    }
+}
